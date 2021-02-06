b6f25b0917d9d8983594d1b27800b2c5ca0eb02f
egluzl <im.hxy@outlook.com>
2021-02-06 18:17:55 +0800

    fix bugs

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,11 @@
-const fs = require('fs')
-const path = require('path')
-const webpack = require('webpack')
-
+const path = require('path');
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                'Vue': ['vue', 'default'],
-                'G': [path.resolve(path.join(__dirname, 'src', 'G', 'index.js')), 'default'],
-            }),
-            // new webpack.ProgressPlugin(percentage => {
-            //     percentage === 1 ? console.log('编译完成：100.00%') : console.log(`编译进度：${(percentage * 100).toFixed(2)}%`)
-            // })
-        ]
+    configureWebpack:{
+        resolve: {
+            alias: {
+                '@': resolve('src')
+            }
+        },
     },
     chainWebpack: config => {
         config.module.rule('vue').use('vue-loader').loader('vue-loader').tap(options => {
@@ -28,4 +21,8 @@ module.exports = {
             return options
         })
     }
+}
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
 }

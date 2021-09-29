aa3f5f6109cfc1eba61ac562a65770439943df83
eGluZl <im.hxy@outlook.com>
2021-09-29 13:36:06 +0800

    add prefix

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,13 +1,13 @@
 //app
-export const SET_LOGGED = 'SET_LOGGED'
+export const SET_LOGGED = 'app/SET_LOGGED'
 
-export const SET_LOGIN_FAIL = 'SET_LOGIN_FAIL'
+export const SET_LOGIN_FAIL = 'app/SET_LOGIN_FAIL'
 
-export const SET_TOKEN = 'SET_TOKEN'
+export const SET_TOKEN = 'app/SET_TOKEN'
 
-export const SET_LOCATION_INFO = 'SET_LOCATION_INFO'
+export const SET_LOCATION_INFO = 'app/SET_LOCATION_INFO'
 
-export const SET_NAV_BAR_HEIGHT = 'SET_NAV_BAR_HEIGHT'
+export const SET_NAV_BAR_HEIGHT = 'app/SET_NAV_BAR_HEIGHT'
 
 export const SET_QUERY = 'app/SET_QUERY'
 
